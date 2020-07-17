c8c90e8f6f4f8b0b2ce2c37e1f7f9f44982e01e8
ic-xu <1309095142@qq.com>
2020-07-17 22:48:42 +0800

    'f'

--- common/src/main/java/com/common/mvc/banner/entity/Banner.java
+++ common/src/main/java/com/common/mvc/banner/entity/Banner.java
@@ -1,5 +1,6 @@
 package com.common.mvc.banner.entity;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 
@@ -9,12 +10,16 @@ public class Banner {
     @Id
     private long id;
 
+    @ApiModelProperty("图片点击地址")
     private String url;
 
+    @ApiModelProperty("图片路径")
     private String imagePath;
 
+    @ApiModelProperty("图片显示标题")
     private String title;
 
+    @ApiModelProperty("图片状态")
     private int status;
 
 }
